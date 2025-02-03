50d1d644225f6de1f01fe749673ca9bb41527d13
hong66489 <483242964@qq.com>
2025-02-03 22:00:26 +0800

    Update add_turboacc.sh

--- add_turboacc.sh
+++ add_turboacc.sh
@@ -49,12 +49,12 @@ if [ -d "./package/turboacc" ]; then
 fi
 
 git clone --depth=1 --single-branch https://github.com/fullcone-nat-nftables/nft-fullcone "$TMPDIR/turboacc/nft-fullcone" || exit 1
-git clone --depth=1 --single-branch https://github.com/chenmozhijin/turboacc "$TMPDIR/turboacc/turboacc" || exit 1
+git clone --depth=1 --single-branch https://github.com/hong66489/turboacc "$TMPDIR/turboacc/turboacc" || exit 1
 if [ -n "$LOCAL_PACKAGE" ]; then
     echo "Using local package: $LOCAL_PACKAGE"
     cp -RT "$LOCAL_PACKAGE" "$TMPDIR/package" || exit 1
 else
-    git clone --depth=1 --single-branch --branch "package" https://github.com/chenmozhijin/turboacc "$TMPDIR/package" || exit 1
+    git clone --depth=1 --single-branch --branch "package" https://github.com/hong66489/turboacc "$TMPDIR/package" || exit 1
 fi
 
 cp -r "$TMPDIR/turboacc/turboacc/luci-app-turboacc" "$TMPDIR/turboacc/luci-app-turboacc"
